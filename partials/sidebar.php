6825ab7ab745e872b26cb30be0c041ff53000fcb
mikahikaai <mikahikaai@gmail.com>
2022-02-14 13:50:26 +0800

    change photo & username

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -11,10 +11,10 @@
     <!-- Sidebar user panel (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
       <div class="image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        <img src="dist/img/user9-128x128.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
       <div class="info">
-        <a href="#" class="d-block">Alexander Pierce</a>
+        <a href="www.facebook.com/rsonemoretime" class="d-block">Rian Septiadi</a>
       </div>
     </div>
 
